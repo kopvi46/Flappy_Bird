7e1e92716e01eb86c61671c4eefb2ed97e6fe01b
kopvi46 <140708770+kopvi46@users.noreply.github.com>
2024-09-06 10:14:05 +0300

    fully workable game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,39 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Transform _obstaclePrafab;
+    [SerializeField] private RectTransform _loseScreen;
+    [SerializeField] private TextMeshProUGUI _playerScoreVisual;
     [SerializeField] private float _gameSpeed;
+    [SerializeField] private int _obstacleSpacingX = 4;
+    [SerializeField] private int _obstacleSpacingY = 1;
 
     private Queue<Transform> _obstaclesQueue = new Queue<Transform>();
+    private int _playerScore;
     private int _queueLength = 7;
-    private int _spacingX = 4;
-    private int _spacingY = 1;
     private int _halfMapSize = 10;
     private float _obstacleReplacmentTimer;
     private float _obstacleReplacmentFrequency;
-    System.Random _randomYSpacing;
+    private System.Random _randomYSpacing;
 
     private void Start()
     {
         _randomYSpacing = new System.Random();
 
-        _obstacleReplacmentTimer = -_halfMapSize / _gameSpeed;
-        _obstacleReplacmentFrequency = _spacingX / _gameSpeed;
-
-        int spacing = 0;
+        _obstacleReplacmentFrequency = _obstacleSpacingX / _gameSpeed;
 
         for (int i = 0; i < _queueLength; i++)
         {
-            Transform t;
-
-            _obstaclesQueue.Enqueue(t = Instantiate(_obstaclePrafab, transform));
-
-            t.position = new Vector3(spacing, _randomYSpacing.Next(-_spacingY, _spacingY), 0);
-            spacing += _spacingX;
+            _obstaclesQueue.Enqueue(Instantiate(_obstaclePrafab, transform));
         }
+
+        StartNewGame();
+
+        Player.Instance.OnPlayerScored += Player_OnPlayerScored;
+        Player.Instance.OnPlayerHitObstacle += Player_OnPlayerHitObstacle;
+    }
+
+    private void Player_OnPlayerScored(object sender, System.EventArgs e)
+    {
+        _playerScore++;
+        _playerScoreVisual.text = "Player score: " + _playerScore.ToString();
+    }
+
+    private void Player_OnPlayerHitObstacle(object sender, System.EventArgs e)
+    {
+        Time.timeScale = 0f;
+
+        _loseScreen.gameObject.SetActive(true);
     }
 
     private void Update()
@@ -47,9 +61,33 @@ public class GameManager : MonoBehaviour
             Transform t = _obstaclesQueue.Dequeue();
             _obstaclesQueue.Enqueue(t);
 
-            t.position = new Vector3(t.position.x + (_spacingX * _queueLength), _randomYSpacing.Next(-_spacingY, _spacingY), 0);
+            t.position = new Vector3(t.position.x + (_obstacleSpacingX * _queueLength), _randomYSpacing.Next(-_obstacleSpacingY, _obstacleSpacingY), 0);
 
             _obstacleReplacmentTimer = 0;
         }
+    }
+
+    public void StartNewGame()
+    {
+        _loseScreen.gameObject.SetActive(false);
+
+        _playerScore = 0;
+        _playerScoreVisual.text = "Player score: " + _playerScore.ToString();
+
+        Time.timeScale = 1f;
+
+        _obstacleReplacmentTimer = -_halfMapSize / _gameSpeed;
+
+        transform.position = Vector3.zero;
+
+        int spacing = 0;
+
+        foreach (Transform t in _obstaclesQueue)
+        {
+            t.position = new Vector3(spacing, _randomYSpacing.Next(-_obstacleSpacingY, _obstacleSpacingY), 0);
+            spacing += _obstacleSpacingX;
+        }
+
+        Player.Instance.GoToStartPosition();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
+    public event EventHandler OnPlayerScored;
+    public event EventHandler OnPlayerHitObstacle;
+
     [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _obstacleLayer;
+    [SerializeField] private LayerMask _passLayer;
 
     private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
@@ -15,10 +28,31 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeScale != 0)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0f);
             _rigidbody.AddForce(transform.up * _jumpForce);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((1 << collision.gameObject.layer) == _obstacleLayer.value)
+        {
+            OnPlayerHitObstacle?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((1 << other.gameObject.layer) == _passLayer.value)
+        {
+            OnPlayerScored?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void GoToStartPosition()
+    {
+        transform.position = new Vector3(-5, 0, 0);
     }
 }
